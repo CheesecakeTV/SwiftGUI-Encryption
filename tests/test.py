10a0385e94259920d0e437b232f117a17eaa89a0
Eric <cheesecaketv53@gmail.com>
2026-03-20 14:24:02 +0100

    0.0.3: Popup create-password

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,15 @@
 import SwiftGUI_Encryption as sge
+import SwiftGUI as sg
 
-my_key = "Hallo Welt"
-
-my_file = sge.sg.PasswordJSONDictFile("filetest/hallo.secret", my_key)
-
-#my_file["Hallo"] = "Welt"
-
-print(my_file)
-print(my_file.key)
+sg.Themes.FourColors.Emerald()
+print(sge.sg.popup_create_password(
+    "PW erstellen",
+    #min_length= 10,
+    must_include_special=True,
+    must_include_lower=True,
+    must_include_upper=True,
+    must_include_digits=True,
+    min_length=5,
+))
 
 
